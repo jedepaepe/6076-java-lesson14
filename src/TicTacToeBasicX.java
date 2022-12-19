017f0d84ea0ffe714c77d73f7714c5072989ef5b
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-12-19 23:03:28 +0100

    squelette pour un tic tac toe

---
+++ src/TicTacToeBasicX.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class TicTacToeBasicX {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[][] state = {
+                { '-', '-', '-' },
+                { '-', '-', '-' },
+                { '-', '-', '-' },
+        };
+
+        do {
+            showBoard(state);
+            doStep(state, scanner);
+        } while(! isTerminated(state));
+        showResult(state);
+    }
+
+    private static void showBoard(char[][] state) {
+        for (int i = 0; i < 3; ++i) {
+            System.out.println("" + state[i][0] + state[i][1] + state[i][2]);
+        }
+    }
+
+    private static void doStep(char[][] state, Scanner scanner) {
+        int line = scanner.nextInt();
+        int column = scanner.nextInt();
+        state[line][column] = 'X';
+    }
+
+    private static boolean isTerminated(char[][] state) {
+        return false;
+    }
+
+    private static void showResult(char[][] state) {
+    }
+}
